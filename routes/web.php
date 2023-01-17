74db2b3d454c8b31c009c3e28e6acf7075c67a85
Damián de Vera <ddevhor@gmail.com>
2023-01-17 14:05:17 +0100

    a32

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('community', [App\Http\Controllers\CommunityLinkController::class, 'index']);
+Route::post('community', [App\Http\Controllers\CommunityLinkController::class, 'store']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
