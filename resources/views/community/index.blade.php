62863241e045e2eaf108f8191cb4b37fac31dbd8
Damián de Vera <ddevhor@gmail.com>
2023-01-23 17:57:49 +0000

    a32

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -1,6 +1,12 @@
+@extends('layouts.app')
+@section('content')
+<h1>Community</h1>
+
+
 @foreach ($links as $link)
 <small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small>
 <li>{{$link->title}}</li>
 
 @endforeach
 {{$links->links()}}
+@stop
